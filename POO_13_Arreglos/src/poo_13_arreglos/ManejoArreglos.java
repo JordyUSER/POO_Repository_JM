88a5ea926ed463b59a645f5c9bf46051d3673b80
JordyUSER <jpmejia9@espe.edu.ec>
2023-05-29 17:54:43 -0500

    Código de la evaluacion y practica 29_Mayo completo

--- POO_13_Arreglos/src/poo_13_arreglos/ManejoArreglos.java
+++ POO_13_Arreglos/src/poo_13_arreglos/ManejoArreglos.java
@@ -5,8 +5,8 @@ import javax.swing.JOptionPane;
  * @author Jordy Mejia
  */
 public class ManejoArreglos {
-    int nNumeros = 0, arrNums[], aux=0, prom=0;
-    String nNombres = "";
+    int nNumeros = 0, arrNums[], aux=0, prom=0, nNombres =0;
+    String arrNombres[];
     public int getnNumeros() {
         return nNumeros;
     }
@@ -18,7 +18,13 @@ public class ManejoArreglos {
     public void establecerArr() {
         arrNums = new int[this.nNumeros];
         for(int i=0; i<this.nNumeros; i++){
-            arrNums[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
+            arrNums[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero en el rango (0-20): "));
+            if ((arrNums[i]>=0) && (arrNums[i]<=20)){
+                continue;
+            } else {
+                System.out.println("Ingrese nuevamente el dato entre 0 y 20...");
+                i--;
+            }
         }
     }
 
@@ -35,17 +41,53 @@ public class ManejoArreglos {
     }
     
     public int sumaArrNums() {
-        for(int i: arrNums){
+        aux = 0;
+        for(int i=0; i < this.nNumeros; i++){
             aux += arrNums[i];
         }
         return aux;
     }
     
     public int promedioNums(){
-        for(int i: arrNums){
+        prom = 0;
+        for(int i=0; i<this.nNumeros; i++){
             prom += arrNums[i];
         }
-        prom = prom/this.nNumeros;
+        prom = prom/arrNums.length;
         return prom;
     }
+
+    public int arrSize() {
+        return arrNums.length;  
+    }
+
+    public void setnNombres(int nNombres) {
+        this.nNombres = nNombres;
+    }
+
+    public void verPositive() {
+        if (this.arrNums[1] > 0) {
+            System.out.println("El numero del arreglo en la posicion 1 es positivo");
+        } else {
+            System.out.println("El numero del arreglo en la posicion 1 es negativo");
+        }
+    }
+
+    public void establecerArrNombres() {
+        arrNombres = new String [this.nNombres];
+        for(int i=0; i<this.nNombres; i++){
+            arrNombres[i] = JOptionPane.showInputDialog("Ingrese un nombre: ");
+        }
+    }
+
+    public int arrNombresSize() {
+        return arrNombres.length;  
+    }
+
+    public void mostrarArrN() {
+        for(int i=0; i<(arrNombres.length);i++){
+            System.out.print("El numero en la posicion " + i + " es: " + arrNombres[i] + "\n");
+        }
+    }
 }
+
